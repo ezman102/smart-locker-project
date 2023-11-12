4939f9acd2e33571c195aacc83fbed6a67807fea
SIN Yuk Yuen <yysin8-c@my.cityu.edu.hk>
2023-11-12 15:57:13 +0800

    Update

---
+++ public/js/welcome.js
@@ -0,0 +1,21 @@
+// welcome.js
+function showWelcomePage() {
+    const contentDiv = document.querySelector('.content');
+    const greeting = getGreetingByTime();
+
+    contentDiv.innerHTML = `
+        <div class="welcome-container">
+            <h2 class="welcome-title">${greeting}, ${displayName}!</h2>
+            <p class="welcome-message">Your locker number: [ ${lockerNumber} ]</p>
+            <img src="../images/locker.jpg" alt="locker" class="welcome-image">
+        </div>
+    `;
+}
+
+
+function getGreetingByTime() {
+    const hour = new Date().getHours();
+    if (hour < 12) return 'Good Morning';
+    if (hour < 18) return 'Good Afternoon';
+    return 'Good Evening';
+}
